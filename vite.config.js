7ab9533068ae4b9bfa51d04a1a92ebc89c957b2c
LAPTOP-RA71B1NR\riccc <ricc.celani@gmail.com>
2026-05-03 18:03:11 +0200

    aggiornamento config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(),    tailwindcss(),
+],
   server: {
     watch: {
       usePolling: true,
